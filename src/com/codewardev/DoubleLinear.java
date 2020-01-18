2e19e783c8a0ee48d3f8a4e8f476081de9c1436f
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-18 22:48:06 +1100

    started

---
+++ src/com/codewardev/DoubleLinear.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/5672682212c8ecf83e000050/train/java
+
+public class DoubleLinear {
+
+	public static int dblLinear(int n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
